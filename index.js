281d615bf16f4cd7f46608887da1a63003f2090b
Ninjax11000 <jamiljoy007@gmail.com>
2023-05-02 17:52:17 +0600

    added response for each chef

--- index.js
+++ index.js
@@ -5,14 +5,23 @@ const port = process.env.PORT || 5000;
 
 
 const chefs =require('./data/chefs.json');
+const recipes=require('./data/recipes.json');
 app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Server is running!!')
-})
+});
 app.get('/chefs', (req, res) => {
   res.send(chefs);
-})
+});
+
+app.get("/recipes/:id", (req, res) => {
+  const id = req.params.id;
+  
+    const chefRecipe = recipes.filter((n) => n.chef_id== id);
+    res.send(chefRecipe);
+  
+});
 
 app.listen(port, () => {
   console.log(` app listening on port ${port}`)
